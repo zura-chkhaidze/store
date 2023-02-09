0d92fa8328705854f2aaadc2f91b5c88093f1789
zura-chkhaidze <zura.chkhaidze444@gmail.com>
2023-02-09 22:09:47 +0400

    Admin Configuration

--- store/models.py
+++ store/models.py
@@ -1,10 +1,11 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+
 # Create your models here.
 class Category(models.Model):
-    name = models.CharField(max_length=255,db_index=True)
-    slug = models.SlugField(max_length=225,unique=True)
+    name = models.CharField(max_length=255, db_index=True)
+    slug = models.SlugField(max_length=225, unique=True)
 
     class Meta:
         verbose_name_plural = 'categories'
@@ -14,25 +15,22 @@ class Category(models.Model):
 
 
 class Product(models.Model):
-    category =models.ForeignKey(Category,related_name='product',on_delete=models.CASCADE)
-    created_by = models.ForeignKey(User,on_delete=models.CASCADE,related_name='product_creator')
-    title = models.CharField(max_length=255,default='admin')
+    category = models.ForeignKey(Category, related_name='product', on_delete=models.CASCADE)
+    created_by = models.ForeignKey(User, on_delete=models.CASCADE, related_name='product_creator')
+    title = models.CharField(max_length=255)
     description = models.TextField(blank=True)
+    author = models.CharField(max_length=255, default='admin')
     image = models.ImageField(upload_to='images/')
-    slug =models.SlugField(max_length=255)
-    price =models.DecimalField(max_digits=4,decimal_places=2)
-    in_stock =models.BooleanField(default=True)
+    slug = models.SlugField(max_length=255)
+    price = models.DecimalField(max_digits=4, decimal_places=2)
+    in_stock = models.BooleanField(default=True)
     is_active = models.BooleanField(default=True)
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
 
     class Meta:
         verbose_name_plural = 'Products'
-        ordering =('-created',)
-
+        ordering = ('-created',)
 
     def __str__(self):
         return self.title
-    
-
-        
